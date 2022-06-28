b632982a64773a3d8829621ad2fb5c6668c55bde
Jose Anthony Jovellanos <jove0610@gmail.com>
2022-06-28 18:48:39 +0800

    install mui and prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
     <header>
+      <CssBaseline />
       <h1>Hello World</h1>
     </header>
   );
