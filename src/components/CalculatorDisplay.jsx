daff8ccc37a4ccb9c5ff8e1b3c5e77d55d2d43a4
Jose Anthony Jovellanos <jove0610@gmail.com>
2022-06-28 23:32:44 +0800

    create calculator functionality

--- src/components/CalculatorDisplay.jsx
+++ src/components/CalculatorDisplay.jsx
@@ -1,22 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Box, Grid, Typography } from '@mui/material';
+import { Grid, Stack, Typography } from '@mui/material';
 
-function CalculatorDisplay({ children }) {
+const IntlFormat = new Intl.NumberFormat('en-us', {
+  maximumFractionDigits: 0,
+});
+
+const formatNum = (num) => {
+  if (num === '') return null;
+  const [integer, decimal] = num.split('.');
+
+  if (decimal) {
+    return `${IntlFormat.format(integer)}.${decimal}`;
+  }
+
+  return IntlFormat.format(integer);
+};
+
+function CalculatorDisplay({ currentNum, prevNum, operation }) {
   return (
     <Grid item xs={12}>
-      <Box p="2em 0.8em 0.5em 0.8em" sx={{ background: '#1c191c' }}>
-        <Typography variant="h5" textAlign="right" sx={{ color: '#fff' }}>
-          {children}
+      <Stack
+        px="0.8em"
+        py="0.5em"
+        justifyContent="space-between"
+        alignItems="space-between"
+        height="100%"
+        spacing="0.5em"
+        sx={{ background: '#1c191c' }}
+      >
+        <Typography
+          textAlign="right"
+          sx={{ color: '#fff', wordWrap: 'break-word' }}
+        >
+          {operation && `${formatNum(prevNum)} ${operation}`}
+          {!operation && <>&nbsp;</>}
         </Typography>
-      </Box>
+
+        <Typography
+          variant="h5"
+          textAlign="right"
+          sx={{ color: '#fff', wordWrap: 'break-word' }}
+        >
+          {formatNum(currentNum) || <>&nbsp;</>}
+        </Typography>
+      </Stack>
     </Grid>
   );
 }
 
 CalculatorDisplay.propTypes = {
-  children: PropTypes.node.isRequired,
+  currentNum: PropTypes.string.isRequired,
+  prevNum: PropTypes.string.isRequired,
+  operation: PropTypes.string.isRequired,
 };
 
 export default CalculatorDisplay;
